a3d060ffbd888a32aada0091c4d917357c96f8c8
simongr1 <135616995+simongr1@users.noreply.github.com>
2023-06-05 14:46:28 +0200

    Add files via upload

---
+++ Schweissen.m
@@ -0,0 +1,51 @@
+clc
+clear
+close all
+%%%%LOG%%%%%
+%Prozentuale verteilung der Leitung hinzugefügt
+
+
+%%%%Konstanten%%%%
+P=21 *1e6; %Leistung in Watt
+
+%%%%%Variablen%%%%%%
+KA= 1.1; %Anwendungsfaktor
+alpha_wd=0.5; % Dynamischer Abminderungsfaktor
+beta= 1; %Eigenspannungsfaktor( bei rundnähten 1)
+Sd=2; %Sicherheitsfaktor (Normal:2)
+alpha_0=0.8; %= Schweißnahtgütefaktor (Güteklasse B)
+
+
+n=14050; %Drehzahl
+r_aussen=209.8;
+r_innen=203.87;
+anzuschweissende_stufe=3; %Es wird immer die mit der kleineren nummer verwendet bsp 5-6 -> 5
+
+
+
+
+
+
+%Materialwerte
+tao_zul=382;%MPa
+dichte=4.5;%g/cm^3
+pzul= 600; %Mpa
+
+%Annahme: Durchgeschweißte V-Naht, über die gesamte dicke des Materials
+%%%Berechnung%%%%
+%Aw=pi*(r_aussen^2-r_innen^2);
+n=n/60; %in sekunden umrechen
+P=P*(anzuschweissende_stufe/10);
+Moment=P/(2*pi*n);
+Moment=Moment*1000;%In Nmm umrechnen
+W_wb=(pi*(r_aussen^4- r_innen^4))/(32*r_aussen);
+W_wt=2*W_wb;
+
+tao_wt=Moment/W_wt;
+
+tao_w_zul=alpha_wd*alpha_0*beta*(tao_zul/Sd); %Dynamische Festigkeit
+
+disp("Sicherheit Schweißnaht")
+
+sicherheit=tao_w_zul/tao_wt
+
